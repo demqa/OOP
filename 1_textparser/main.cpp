90f09020b09b78be17c1b254e544de3cc5d20195
Dmitry 'demqa' Stepanov <greatsteamguard@gmail.com>
2023-02-01 16:23:57 +0300

    1. add textparser

---
+++ 1_textparser/main.cpp
@@ -0,0 +1,32 @@
+/*
+ Word frequency
+
+ stdin / file
+
+ text
+
+ words:
+ russian
+ english
+ c-word
+ numbers
+
+ dictionary: (hashtable)
+ {word: {counter, [line1, line2, line3]}
+
+ dictionary.out() -> text/.json/html
+*/
+
+int main()
+{
+
+    /*
+    for each file{
+        f = dlopen();
+        dlsym(f, "getdescr");
+    }
+    */
+
+
+    return 0;
+}
---
+++ 1_textparser/word.h
@@ -0,0 +1,12 @@
+#ifndef WORD_H_
+#define WORD_H_
+
+/*
+struct word_description
+{
+    char name[10] = "some_name";
+    char func[16] =
+};
+*/
+
+#endif // WORD_H_
